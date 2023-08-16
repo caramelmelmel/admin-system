e29587e8611dda85c24ed99b60d25395da7ed061
caramelmelmel <melodylyq262@gmail.com>
2023-08-16 11:56:46 +0800

    fix: object ref teacher and stu

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,17 +1,24 @@
+const { isEmptyEntry, getRegisterViaEmail, isValidEmail } = require("../helpers/objectUtils");
 const express = require('express');
 const router = express.Router();
 
 module.exports = (studentService) => {
   router.post('/', async (req, res) => {
-    try {
       const { email, name } = req.body;
-      
-      const student = await studentService.addStudent(email, name);
-      res.status(201).json(student);
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({ message: 'Error adding student' });
+      if (isEmptyEntry(email)&& isEmptyEntry(name)) {
+        return res.status(400).json({ message: 'Empty entries' });
     }
+    if (!isValidEmail(email)) {
+      return res.status(401).json({message: 'Email format should be name@example.com'})
+    }
+      const studentEmail = await getRegisterViaEmail(email,studentService.studentModel);
+      
+    if (studentEmail && !(studentEmail instanceof Error)) {
+        return res.status(409).json({ message: 'Student with this email already exists' });
+    }
+
+      const student = await studentService.addStudent(email, name);
+      return res.status(201).json(student);
   });
 
   router.get('/', async (req, res) => {
--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -1,16 +1,22 @@
+const {getRegisterViaEmail,isEmptyEntry} = require('../helpers/objectUtils')
 const express = require('express');
 const router = express.Router();
 
 module.exports = (teacherService) => {
   router.post('/', async (req, res) => {
-    try {
+
       const { email, name } = req.body; 
-      const teacher = await teacherService.addTeacher(email, name);
-      res.status(201).json(teacher);
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ message: 'Error adding teacher' });
+      if (isEmptyEntry(email)&& isEmptyEntry(name)) {
+        return res.status(400).json({ message: 'Bad request' });
     }
+    const TeacherEmail = await getRegisterViaEmail(email,teacherService.teacherModel);
+    console.log(`teacher email is ${TeacherEmail}`)
+    if (TeacherEmail) {
+        return res.status(409).json({ message: 'Teacher with this email already exists' });
+    }
+      const teacher = await teacherService.addTeacher(email, name);
+      return res.status(201).json(teacher);
+    
   });
 
   router.get('/', async (req, res) => {
