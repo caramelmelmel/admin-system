1e7794c0c81018a9f2344162993f35d7103a7b03
caramelmelmel <melodylyq262@gmail.com>
2023-08-16 20:39:18 +0800

    test(object utils): tested object utils

--- test/objectUtils.test.js
+++ test/objectUtils.test.js
@@ -1,14 +1,29 @@
-const { isEmptyEntry } = require("../helpers/objectUtils")
+const { isEmptyEntry, isValidEmail } = require("../helpers/objectUtils")
 
 describe('Check for empty properties', ()=> {
     test('Empty string',()=> {
         expect(isEmptyEntry('')).toBeTruthy();
     })
     test('Valid non empty string',()=> {
-        expect(isEmptyEntry("melmelmel")).not.toBeFalsy();
+        expect(isEmptyEntry("melmelmel")).toBeFalsy();
+    })
+    test('Array object empty',()=> {
+        expect(isEmptyEntry([])).toBeTruthy();
     })
     test('Null object',()=> {
         expect(isEmptyEntry(null)).toBeTruthy();
+    })
+})
+
+describe('Validate email Format', ()=> {
+    test('Empty email',()=> {
+        expect(isValidEmail('')).toBeFalsy();
+    })
+    test('invalid input',()=> {
+        expect(isValidEmail("melmelmel")).toBeFalsy();
+    })
+    test('valid email',()=> {
+        expect(isValidEmail("melmelmel@mail.com")).toBeTruthy();
     })
 })
 
